668b5035874097f4c90a24084114fb94c18259cc
laipi <laipi.ee10@nycu.edu.tw>
2024-07-23 20:13:23 +0800

    complete service sample

---
+++ catkin_ws/src/roslibpy_project/src/call_service.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import roslibpy
+
+client = roslibpy.Ros(host='localhost', port=9090)
+client.run()
+
+service = roslibpy.Service(client, '/set_ludicrous_speed', 'std_srvs/SetBool')
+request = roslibpy.ServiceRequest({'data':True})
+
+print('Calling service...')
+result = service.call(request)
+print('Service response: {}'.format(result))
+
+client.terminate()
